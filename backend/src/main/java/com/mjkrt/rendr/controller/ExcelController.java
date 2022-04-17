d993c98aa674cbae8fc0811d158b7a3ce1fbf60a
muhammad-khair <muhammad_khair@outlook.com>
2022-04-18 01:48:34 +0800

    Remove unused import

--- backend/src/main/java/com/mjkrt/rendr/controller/ExcelController.java
+++ backend/src/main/java/com/mjkrt/rendr/controller/ExcelController.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.mjkrt.rendr.entity.DataTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
